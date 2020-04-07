261e79b69428b6f936f0e36755604da95f37e361
mateusz.pachulski <mateusz.pachulski@famoc.com>
2020-04-07 21:03:15 +0200

    added navigation component?

--- app/src/main/java/com/example/github/feature/MainActivity.kt
+++ app/src/main/java/com/example/github/feature/MainActivity.kt
@@ -4,10 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.github.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 }
