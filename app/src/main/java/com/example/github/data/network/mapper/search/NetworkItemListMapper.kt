54bd089f0f9b0aa81f86ef607b284b1ca1da411f
trOnk12 <m.pachulski94@gmail.com>
2020-04-14 16:37:16 +0200

    extracted data source logic to RepositorySourceManager

---
+++ app/src/main/java/com/example/github/data/network/mapper/search/NetworkItemListMapper.kt
@@ -0,0 +1,4 @@
+package com.example.github.data.network.mapper.search
+
+class NetworkItemListMapper {
+}
---
+++ app/src/main/java/com/example/github/data/network/source/RepositorySourceManager.kt
@@ -0,0 +1,4 @@
+package com.example.github.data.network.source
+
+class RepositorySourceManager {
+}
